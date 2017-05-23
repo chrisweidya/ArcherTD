ed33535e6b9171e3e12f072583920d7416583243
Christopher Weidya <chrisweidya@hotmail.com>
2017-05-23 11:19:13 +0800

    Network 2 player movement (follow tutorial)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float _turnInput = 0;
+    private float _moveInput = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetInputs();
+        UpdateMovement();
+        print("fads"); 
+	}
+
+    private void GetInputs() {
+        _turnInput = Input.GetAxis("Horizontal");
+        _moveInput = Input.GetAxis("Vertical");
+    }
+
+    private void UpdateMovement() {
+        transform.Rotate(0, _turnInput * Time.deltaTime * 150f, 0);
+        transform.Translate(0, 0, _moveInput * Time.deltaTime * 3f);
+    }
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+
+    private static Singleton _instance;
+
+    public static Singleton Instance { get { print("get"); return _instance; } }
+
+    private void Awake() {
+        print("created singelton");
+        if(_instance != null && _instance != this) {
+            Destroy(this.gameObject);
+        }
+        else {
+            _instance = this;
+        }
+    }
+	
+}
