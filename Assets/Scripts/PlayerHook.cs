a334102e703f261cb32af108a0028fae76eb3892
Christopher Weidya <chrisweidya@hotmail.com>
2017-06-13 15:04:08 +0800

    allegiance implementation

---
+++ Assets/Scripts/PlayerHook.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Prototype.NetworkLobby {
+    public class PlayerHook : LobbyHook {
+
+        public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer) {
+            LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+            PlayerProperties playerProperties = gamePlayer.GetComponent<PlayerProperties>();
+
+            playerProperties.SetTeam(lobby.playerFaction);
+            print("set " + lobby.playerFaction);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerProperties.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperties : MonoBehaviour {
+    [SerializeField]
+    private string team = "";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetTeam(string team) {
+        this.team = team;
+    }
+
+    public string GetTeam() {
+        return team;
+    }
+}
