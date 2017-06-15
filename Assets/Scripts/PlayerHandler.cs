e1e53147b7a35a9809078b6f3fd4b6e16d3e3d43
Christopher Weidya <chrisweidya@hotmail.com>
2017-06-15 17:52:37 +0800

    unparented dead player warden prefab

--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -82,6 +82,7 @@ public class PlayerHandler : NetworkBehaviour {
 
     private void OnIsDead(bool isDead) {
         if (isDead) {
+            transform.parent = null;
             EventManager.FirePlayerStateChange(PlayerState.Death, this.netId);
         }
     }
