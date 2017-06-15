661005af7aa8f9d664198c6c82f6c53eaac87332
Christopher Weidya <chrisweidya@hotmail.com>
2017-06-15 14:43:20 +0800

    health reduction function checks that both player is alive before execution

--- Assets/Scripts/HealthNetwork.cs
+++ Assets/Scripts/HealthNetwork.cs
@@ -27,16 +27,11 @@ public class HealthNetwork : NetworkBehaviour {
     {
         EventManager.TakeDamageAction -= ReduceHealth;
     }
-    // Use this for initialization
+
     void Start () {
         playerProps = GetComponent<PlayerProperties>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void ReduceHealth(float dmg, NetworkInstanceId victimNetId)
     {
         if (isLocalPlayer) {
@@ -53,25 +48,18 @@ public class HealthNetwork : NetworkBehaviour {
     private void CmdReduceHealth(float dmg, NetworkInstanceId victimNetId, NetworkInstanceId killerNetId)
     {
         GameObject playerGO = NetworkServer.FindLocalObject(victimNetId);
-        
-        PlayerHandler playerHandler = playerGO.GetComponent<PlayerHandler>();
-        if (!playerHandler.GetIsDead()) {
+        PlayerHandler killerPlayerHandler = NetworkServer.FindLocalObject(killerNetId).GetComponent<PlayerHandler>();
+        PlayerHandler victimPlayerHandler = playerGO.GetComponent<PlayerHandler>();
+
+        if (!killerPlayerHandler.GetIsDead() && !victimPlayerHandler.GetIsDead()) {
             HealthNetwork affectedHealth = playerGO.GetComponent<HealthNetwork>();
             affectedHealth.currentHealth -= dmg;
             Debug.Log("Cmd health " + currentHealth);
             if (affectedHealth.currentHealth <= 0) {
-                playerHandler.SetIsDead(true);
+                victimPlayerHandler.SetIsDead(true);
             }
         }
     }
-    
-
-    //[ClientRpc]
-    //private void RpcReduceHealth(float dmg)
-    //{
-    //    hpBar.SetHealthBar(currentHealth);
-      
-    //}
     
     void OnHealthUpdate(float health)
     {
