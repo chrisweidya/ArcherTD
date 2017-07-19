293afe42aeca93247a48a19eb289abb58f1887db
Sim Wei Jin <simwj@garena.com>
2017-07-19 12:53:24 +0800

    no message

---
+++ Assets/Scripts/AudioAmbienceHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAmbienceHandler : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource source;
+    [SerializeField]
+    private float minRange;
+    [SerializeField]
+    private float maxRange;
+
+    // Use this for initialization
+    void Start () {
+        if (source == null) {
+            source = GetComponent<AudioSource>();
+        }
+        StartCoroutine(RandomAmbience());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator RandomAmbience() {
+
+        while (true) {
+            source.Play();
+            yield return new WaitForSeconds(Random.Range(minRange, maxRange));
+        }
+       
+    }
+}
